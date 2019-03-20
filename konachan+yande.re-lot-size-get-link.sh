5537d05cfa5468979ef68436563927b61342356f
poly000 <1348292515@qq.com>
2019-03-20 21:25:20 +0800

    fixed a bug

--- konachan+yande.re-lot-size-get-link.sh
+++ konachan+yande.re-lot-size-get-link.sh
@@ -3,8 +3,8 @@ a()
 {
 echo 请输入搜索tag的关键词\(输入n跳过搜索\)
 read TAGS
-if [ ! x$TAGS = xn ]
-then if [ x$TAGS = x ]
+if [ ! x${TAGS} = xn ]
+then if [ x${TAGS} = x ]
      then a
      else b
      fi
@@ -13,10 +13,10 @@ fi
 b()
 {
 echo Konachan:
-wget https://konachan.net/tag.json?name=$TAGS -o /dev/null -O -|jq .|grep -i $TAGS|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g
+wget https://konachan.net/tag.json?name=${TAGS} -o /dev/null -O -|jq .|grep -i ${TAGS}|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g
 echo
 echo Yande.re:
-wget https://yande.re/tag.json?name=$TAGS -o /dev/null -O -|jq .|grep -i $TAGS|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g
+wget https://yande.re/tag.json?name=${TAGS} -o /dev/null -O -|jq .|grep -i ${TAGS}|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g
 echo
 echo 需要搜索下一个tag吗？（多tag请用“+”连接）（y/*）
 read -s -n 1 AGAIN
@@ -48,16 +48,21 @@ echo a 所有页数
 read -s -n 1 PAGE
 case $PAGE in
 [Oo])
-wget https://$BOORU/post.json?tags=$TAGS\&page=1 -o /dev/null -O -|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'|dd of=link-list
+wget https://$BOORU/post.json?tags=${TAGS}\&page=1 -o /dev/null -O -|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'|dd of=link-list
 ;;
 [Aa])
 mkdir ___tmp___
 cd ___tmp___
-wget https://$BOORU/post\?tags\=$TAGS -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>PAGE
+wget https://$BOORU/post\?tags\=${TAGS} -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>PAGE
 echo 请输入要下载多少页（最多`cat PAGE`）
 read PAGE_MAX
-echo -e for PAGE in {1..$PAGE_MAX}\\ndo echo https://$BOORU/post.json?tags=$TAGS\\\&page=\$PAGE \>\> List\\ndone|bash
-aria2c -j 15 -i List #--http-proxy= --https-proxy= #请依据网络情况修改-j num
+# echo -e for PAGE in {1..$PAGE_MAX}\\ndo echo https://$BOORU/post.json?tags=$TAGS\\\&page=\$PAGE \>\> List\\ndone|bash
+PAGE=0
+while [ $PAGE -le $PAGE_MAX ]
+do PAGE=$((PAGE+1))
+	echo https://$BOORU/post.json?tags=${TAGS}\&page=$PAGE >> List
+done
+aria2c -j 15 -i List #--http-proxy= --https-proxy= # 请依据网络情况修改-j num
 cat *json|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'>../link-list
 cd ..
 rm -rf ___tmp___
