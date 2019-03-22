575884fac40d199f709dd968e8c0e4671582f151
poly000 <1348292515@qq.com>
2019-03-22 22:16:47 +0800

    lololololo

--- booru-lot-size-get-link.sh
+++ booru-lot-size-get-link.sh
@@ -13,13 +13,30 @@ fi
 b()
 {
 echo Konachan:
-wget https://konachan.net/tag.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g|more
+wget https://konachan.net/tag.json?name=${tags} -o /dev/null -O -|
+jq .|
+grep -i ${tags}|
+sed -s 's\",\\g'|
+sed -s 's\"\\g'|
+sed -s s/name://g|
+more
 echo
 echo Yande.re:
-wget https://yande.re/tag.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g|more
+wget https://yande.re/tag.json?name=${tags} -o /dev/null -O -|
+jq .|grep -i ${tags}|
+sed -s 's\",\\g'|
+sed -s 's\"\\g'|
+sed -s s/name://g|
+more
 echo
 echo Danbooru:
-wget https://danbooru.donmai.us/tags.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed 's\"\\g'|sed -s s/name://g|more
+wget https://danbooru.donmai.us/tags.json?name=${tags} -o /dev/null -O -|
+jq .|
+grep -i ${tags}|
+sed -s 's\",\\g'|
+sed 's\"\\g'|
+sed -s s/name://g|
+more
 echo 需要搜索下一个tag吗？（多tag请用“+”连接）（y/*）
 read -s -n 1 again
 case $again in
@@ -53,13 +70,24 @@ echo a 所有页数
 read -s -n 1 page
 case $page in
 [Oo])
-wget https://$booru.json?tags=${tags}\&page=1 -o /dev/null -O -|jq .|grep \"file_url|sed -s 's/    "file_url": "//g'|sed -s 's/",//g'|dd of=link-list
+wget https://$booru.json?tags=${tags}\&page=1 -o /dev/null -O -|
+jq .|
+grep \"file_url|
+sed -s 's/    "file_url": "//g'|
+sed -s 's/",//g'|
+dd of=link-list
 ;;
 [Aa])
 mkdir ___tmp___
 cd ___tmp___
 if [ $booru != danbooru.donmai.us/posts ]
-then wget https://$booru\?tags\=${tags} -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>page
+then wget https://$booru\?tags\=${tags} -o /dev/null -O - |
+sed -s 's/ /\n/g'|
+grep href|
+tail -n 12|
+sed -s 's/&amp;/\n/g'|
+head -n 1|
+sed -s 's\href="/post?page=\\g'>page
 echo 请输入要下载多少页（最多`cat page`）
 else echo 请输入要下载多少页（最多1000）
 fi
@@ -71,7 +99,11 @@ do page=$((page+1))
 	echo https://$booru.json?tags=${tags}\&page=$page >> List
 done
 aria2c -i List #--http-proxy= --https-proxy=
-cat ${booru#*/}*|jq .|grep \"file_url|sed -s 's/    "file_url": "//g'|sed -s 's/",//g'>../link-list
+cat ${booru#*/}*|
+jq .|
+grep \"file_url|
+sed -s 's/    "file_url": "//g'|
+sed -s 's/",//g'>../link-list
 cd ..
 rm -rf ___tmp___
 ;;
