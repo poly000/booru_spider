0d652ccb1e80d532b47347c38a029c3cb1a9f05a
poly000 <1348292515@qq.com>
2019-03-22 21:56:07 +0800

    supported danbooru

--- konachan+yande.re-lot-size-get-link.sh
+++ konachan+yande.re-lot-size-get-link.sh
@@ -19,7 +19,7 @@ echo Yande.re:
 wget https://yande.re/tag.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed -s 's\"\\g'|sed -s s/name://g|more
 echo
 echo Danbooru:
-wget https://danbooru.donmai.us/tag.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed 's\"\\g'|sed -s s/name://g|more
+wget https://danbooru.donmai.us/tags.json?name=${tags} -o /dev/null -O -|jq .|grep -i ${tags}|sed -s 's\",\\g'|sed 's\"\\g'|sed -s s/name://g|more
 echo 需要搜索下一个tag吗？（多tag请用“+”连接）（y/*）
 read -s -n 1 again
 case $again in
@@ -36,13 +36,13 @@ echo 请选择图站（D/K/Y）
 read -s -n 1 booru
 case $booru in
 [Dd])
-booru=danbooru.donmai.us
+booru=danbooru.donmai.us/posts
 ;;
 [Kk])
-booru=konachan.net
+booru=konachan.net/post
 ;;
 [Yy])
-booru=yande.re
+booru=yande.re/post
 ;;
 esac
 echo 请输入需要的tag（多tag请用“+”连接）
@@ -51,47 +51,29 @@ echo 需要下载？
 echo o 仅第一页，即最新
 echo a 所有页数
 read -s -n 1 page
-if [ $booru != danbooru.donmai.us ]
-then case $page in
+case $page in
 [Oo])
-wget https://$booru/post.json?tags=${tags}\&page=1 -o /dev/null -O -|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'|dd of=link-list
+wget https://$booru.json?tags=${tags}\&page=1 -o /dev/null -O -|jq .|grep \"file_url|sed -s 's/    "file_url": "//g'|sed -s 's/",//g'|dd of=link-list
 ;;
 [Aa])
 mkdir ___tmp___
 cd ___tmp___
-wget https://$booru/post\?tags\=${tags} -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>page
+if [ $booru != danbooru.donmai.us/posts ]
+then wget https://$booru\?tags\=${tags} -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>page
 echo 请输入要下载多少页（最多`cat page`）
+else echo 请输入要下载多少页（最多1000）
+fi
 read page_max
 page=0
+page_max=$((page_max-1))
 while [ $page -le $page_max ]
 do page=$((page+1))
-	echo https://$booru/post.json?tags=${tags}\&page=$page >> List
-done
-aria2c -j 15 -i List #--http-proxy= --https-proxy= # 请依据网络情况修改-j num
-cat *json|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'>../link-list
-cd ..
-rm -rf ___tmp___
-;;
-esac
-else case $page in
-[Oo])
-wget https://$booru/post.json?tags=${tags}\&page=1 -o /dev/null -O -|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'|dd of=link-list
-;;
-[Aa])
-mkdir ___tmp___
-cd ___tmp___
-echo 请输入要下载多少页（最多1000）
-read page_max
-page=0
-while [ $page -le $page_max ]
-do page=$((page+1))
-	echo https://$booru/post.json?tags=${tags}\&page=$page >> List
+	echo https://$booru.json?tags=${tags}\&page=$page >> List
 done
 aria2c -i List #--http-proxy= --https-proxy=
-cat posts*|jq .|grep file_url|sed -s 's/"file_url": "//g'|sed -s 's/",//g'|sed -s 's/    //g'>../link-list
+cat ${booru#*/}*|jq .|grep \"file_url|sed -s 's/    "file_url": "//g'|sed -s 's/",//g'>../link-list
 cd ..
 rm -rf ___tmp___
 ;;
 esac
-fi
 unset booru page page_max tags
