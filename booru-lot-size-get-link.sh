5eea4ed1c3419baec5e12d5b5140281d4cd2e867
poly000 <34085039+poly000@users.noreply.github.com>
2019-03-26 22:41:23 +0800

    Update booru-lot-size-get-link.sh

--- booru-lot-size-get-link.sh
+++ booru-lot-size-get-link.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-a(){
+# v1.0.6
+# 2019年 03月 26日 星期二 22:35:05 CST
+# Poly000
+# 可以爬取booru图链接为链接列表。
+# 需要包：jq aria2 kdialog
+# 本脚本遵循GPL 3.0协议
+# v1.0.6  修复kdialog错误
+# v1.0.5b 使用kdialog
+# v1.0.4  初步支持Danbooru
+# v1.0.3  优化代码 & 变量名小写化
+# v1.0.2  优化代码
+# v1.0.1  修复page_max问题
+# v1.0.0  实现搜索标签
+search_tags(){
 if tags=`kdialog --inputbox 请输入搜索tag的关键词 2>/dev/null`
 then if [ x = x$tags ]
-     then a
+     then search_tags
      fi
 fi
 }
-a
+search_tags
 if [ x != x$tags ]
 then
 	kdialog --msgbox 开始搜索... 2>/dev/null
@@ -33,36 +46,36 @@ then
 fi
 booru=`kdialog --menu 请选择图站 1 Danbooru 2 Konachan 3 Yande.re 2>/dev/null`
 case $booru in
-1)
-booru=danbooru.donmai.us/posts
-;;
-2)
-booru=konachan.net/post
-;;
-3)
-booru=yande.re/post
-;;
+	1)
+	booru=danbooru.donmai.us/posts
+	;;
+	2)
+	booru=konachan.net/post
+	;;
+	3)
+	booru=yande.re/post
+	;;
 esac
 tags=`kdialog --inputbox 请输入需要的tag（多tag请用“+”连接） 2>/dev/null`
 path=`pwd`
 cd `mktemp -td dir.XXXXXXXX`
 if [ $booru != danbooru.donmai.us/posts ]
-then wget https://$booru\?tags\=${tags} -o /dev/null -O - |
-sed -s 's/ /\n/g'|
-grep href|
-tail -n 12|
-sed -s 's/&amp;/\n/g'|
-head -n 1|
-sed -s 's\href="/post?page=\\g'>page
-page_max=`kdialog --inputbox 请输入要下载多少页（默认为最大值）`
-if [ 0$page_max = 0 ]
-then page_max=`cat page`
-fi
-else echo page_max=`kdialog --inputbox 请输入要下载多少页（至多未知，也许120）`
+then 	wget https://$booru\?tags\=${tags} -o /dev/null -O - |
+	sed -s 's/ /\n/g'|
+	grep href|
+	tail -n 12|
+	sed -s 's/&amp;/\n/g'|
+	head -n 1|
+	sed -s 's\href="/post?page=\\g'>page
+	page_max=`kdialog --inputbox 请输入要下载多少页（默认为最大值）`
+	if [ 0$page_max = 0 ]
+	then	 page_max=`cat page`
+	fi
+else 	echo page_max=`kdialog --inputbox 请输入要下载多少页（至多未知，也许120）`
 fi
 page=0
 while [ $page -lt $page_max ]
-do page=$((page+1))
+do 	page=$((page+1))
 	echo https://$booru.json?tags=${tags}\&page=$page >> List
 done
 aria2c -i List #-j num #--http-proxy= --https-proxy= # -j：指定最高同时下载文件数量 （1～n，默认5）
