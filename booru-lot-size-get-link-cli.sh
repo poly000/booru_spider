daf4464fa1bc474688e0c4840295f46f49f3d8a6
Poly000 <1348292515@qq.com>
2019-05-17 13:07:56 +0800

    English ver

---
+++ booru-lot-size-get-link-cli.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Poly000
+# CLI-1.3.1
+
+path=`pwd`
+tempdir=`mktemp -td dir.XXXXXXXX`
+cd $tempdir
+a()
+{
+echo Please type the keyword to search tags\(type n to skip\)
+read tags
+if [ ! x${tags} = xn ]
+then if [ x${tags} = x ]
+     then a
+     else b
+     fi
+fi
+}
+b()
+{
+echo Konachan:
+curl https://konachan.net/tag?name=${tags} 2>/dev/null|grep next_page|sed -s 's/&amp;type=">/\n/g ; s/</\n/g ; s/">/\n/g'|sed -n 29p>tags
+max_tags=`cat tags`
+if [ x$max_tags != x ]
+then	page_tags=0
+	rm tags
+	until [ $page_tags = $max_tags ]
+	do	page_tags=$((page_tags+1))
+		echo https://konachan.net/tag.json?name=${tags}\&page\=$page_tags >> tags
+	done
+	aria2c -i tags # -j num --http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+	cat tag.*|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g'
+	rm tag*
+else	curl https://konachan.net/tag.json?name=${tags} 2>/dev/null|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g'
+	rm tag*
+fi
+echo
+echo Yande.re:
+curl https://yande.re/tag?name=${tags} 2>/dev/null|grep next_page|sed -s 's/&amp;type=">/\n/g ; s/</\n/g ; s/">/\n/g'|sed -n 29p>tags
+max_tags=`cat tags`
+if [ x$max_tags != x ]
+	then	page_tags=0
+			rm tags
+	until [ $page_tags = $max_tags ]
+	do	page_tags=$((page_tags+1))
+		echo https://yande.re/tag.json?name=${tags}\&page\=$page_tags >> tags
+	done
+	aria2c -i tags # -j num --http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+	cat tag.*|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g'
+	rm tag*
+else	curl https://yande.re/tag.json?name=${tags} 2>/dev/null|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g'
+	rm tag*
+fi
+echo
+echo Danbooru:
+curl 'https://danbooru.donmai.us/tags.json?commit=Search&search\[hide_empty\]=yes&search\[name_matches\]=*'${tags}'*&search\[order\]=date&utf8=%E2%9C%93' 2>/dev/null|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g'
+echo do you wish search next tag?（y/*）
+read -s -n 1 again
+case $again in
+[Yy])
+a
+;;
+esac
+}
+a
+echo -e Please select a booru\\nd: Danbooru\\nk: Konachan\\ny: Yande.re
+read -s -n 1 booru
+case $booru in
+[Dd])
+booru=danbooru.donmai.us/posts
+;;
+[Kk])
+booru=konachan.net/post
+;;
+[Yy])
+booru=yande.re/post
+;;
+esac
+echo Please type the tags you need \(use + connect tags\)
+read tags
+echo what filename you wish to save?
+while [ x$outfile = x ]
+do read outfile
+done
+if [ $booru != danbooru.donmai.us/posts ]
+then curl https://$booru\?tags\=${tags} 2>/dev/null|sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>page
+echo Please type how many pages you wish get \(Default: max\)
+read page_max
+  if [ 0$page_max = 0 ]
+  then page_max=`cat page`
+  fi
+else echo Please type how many pages you wish get \(Max: 1000 or others\)
+	read page_max
+fi
+page=0
+while [ $page -lt $page_max ]
+do page=$((page+1))
+	echo https://$booru.json?tags=${tags}\&page=$page >> List
+done
+aria2c -i List #-j num #--http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+cat ${booru#*/}*|sed 's/{/\n{/g ; s/}]/}\n]/g'|
+# grep -v 'rating":"q' | #exclude Questionable
+# grep -v 'rating":"e' | #exclude Explicit
+# grep -v 'rating":"s' | #exclude Safe
+sed 's/,/\n/g'|grep \"file_url|sed 's/"file_url":"//g;s/"//g'>$path/"$outfile"
+rm -rf $tempdir
---
+++ booru-lot-size-get-link.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# v1.3.1
+# Poly000
+# able to get booru pics link as a link list text file.
+temp0=`mktemp -td dir.XXXXXXXX`
+cd $temp0
+function set_booru(){
+	booru=`kdialog --menu Please select a booru 1 Danbooru 2 Konachan 3 Yande.re 2>/dev/null`
+	if [ x$booru != x ]
+	then	case $booru in
+		1)
+		booru=danbooru.donmai.us/posts
+		;;
+		2)
+		booru=konachan.net/post
+		;;
+		3)
+		booru=yande.re/post
+		;;
+		esac
+	else set_booru
+	fi
+}
+function save_file(){
+	path="`kdialog --getsavefilename $HOME "*.txt" 2>/dev/null`"
+	if [ x${path} = x ]
+	then save_file
+	fi
+}
+function search_tags(){
+	if tags=`kdialog --inputbox Please type the keyword to search tags 2>/dev/null`
+	then if [ x = x${tags} ]
+	     then search_tags
+	     fi
+	fi
+	if [ x != x${tags} ]
+	then
+		kdialog --msgbox started to search... 2>/dev/null &
+		temp1=`mktemp -t temp.XXXXXXXX`
+		exec 3> $temp1
+		echo Konachan: >&3
+		wget https://konachan.net/tag?name=${tags} -o /dev/null -O -|grep next_page|sed -s 's/&amp;type=">/\n/g ; s/</\n/g ; s/">/\n/g'|sed -n 29p>tags
+		max_tags=`cat tags`
+		if [ x$max_tags != x ]
+		then	page_tags=0
+			rm tags
+			until [ $page_tags = $max_tags ]
+			do	page_tags=$((page_tags+1))
+				echo https://konachan.net/tag.json?name=${tags}\&page\=$page_tags >> tags
+			done
+			aria2c -i tags # -j num --http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+			cat tag.*|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g' >&3
+			rm tag*
+		else	wget https://konachan.net/tag.json?name=${tags} -o /dev/null -O -|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g' >&3
+			rm tag*
+		fi
+		echo >&3
+		echo Yande.re: >&3
+		wget https://yande.re/tag?name=${tags} -o /dev/null -O -|grep next_page|sed -s 's/&amp;type=">/\n/g ; s/</\n/g ; s/">/\n/g'|sed -n 29p>tags
+		max_tags=`cat tags`
+		if [ x$max_tags != x ]
+		then	page_tags=0
+			rm tags
+			until [ $page_tags = $max_tags ]
+			do	page_tags=$((page_tags+1))
+				echo https://yande.re/tag.json?name=${tags}\&page\=$page_tags >> tags
+			done
+			aria2c -i tags # -j num --http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+			cat tag.*|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g' >&3
+			rm tag*
+		else	wget https://yande.re/tag.json?name=${tags} -o /dev/null -O -|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g' >&3
+			rm tag*
+		fi
+		echo >&3
+		echo Danbooru: >&3
+		wget 'https://danbooru.donmai.us/tags.json?commit=Search&search[hide_empty]=yes&search[name_matches]=*'${tags}'*&search[order]=date&utf8=%E2%9C%93' -o /dev/null -O -|sed 's/,/\n/g'|grep \"name|sed 's/"name":"//g;s/"//g' >&3
+		rm tag*
+		kdialog --textbox $temp1 450 675 2>/dev/null &
+		if kdialog --yesno Do you wish search next tag? 2>/dev/null
+		then	tags=
+			search_tags
+		fi
+	fi
+}
+search_tags
+set_booru
+tags=`kdialog --inputbox Please type the tags you wish \(use + connect tags\) 2>/dev/null`
+if [ $booru != danbooru.donmai.us/posts ]
+then 	wget https://$booru\?tags\=${tags} -o /dev/null -O - |sed -s 's/ /\n/g'|grep href|tail -n 12|sed -s 's/&amp;/\n/g'|head -n 1|sed -s 's\href="/post?page=\\g'>page
+	page_max=`kdialog --inputbox Please type how many pages you wish get \(Default: max\) 2>/dev/null`
+	if ! [ 0 -lt `cat page` ]
+	then	 page_max=1
+	elif [ 0$page_max = 0 ]
+	then	 page_max=`cat page`
+	fi
+else 	page_max=`kdialog --inputbox Please type how many pages you wish get \(Max: 1000 or others\) 2>/dev/null`
+fi
+page=0
+while [ $page -lt $page_max ]
+do 	page=$((page+1))
+	echo https://$booru.json?tags=${tags}\&page=$page >> List
+done
+temp2=`mktemp -t temp.XXXXXXXX`
+kdialog --msgbox 开始获取... 2>/dev/null &
+aria2c -i List #-j num #--http-proxy= --https-proxy= # -j：Set maximum number of parallel downloads for  \(1～n，default 5\)
+cat ${booru#*/}*|sed 's/{/\n{/g ; s/}]/}\n]/g'|
+# grep -v 'rating":"q' | #exclude Questionable
+# grep -v 'rating":"e' | #exclude Explicit
+# grep -v 'rating":"s' | #exclude Safe
+sed 's/,/\n/g'|grep \"file_url|sed 's/"file_url":"//g;s/"//g'>$temp2
+save_file
+cp $temp2 "${path}"
+rm -rf $temp0 $temp1 $temp2
