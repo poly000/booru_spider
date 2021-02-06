4af77f843867da92361a30d93d02e4cfb59f49ae
poly000 <34085039+poly000@users.noreply.github.com>
2021-02-06 11:57:27 +0800

    Update booru-lot-size-get-link-cli.sh

--- booru-lot-size-get-link-cli.sh
+++ booru-lot-size-get-link-cli.sh
@@ -4,10 +4,10 @@
 # The script could use for get anime pictures' URLs for konachan, yande.re, danbooru
 ##########
 # aria2 -> -j：set out how many pictures will concurrent downloading
-http_proxy=
-https_proxy=
+export http_proxy=
+export https_proxy=
 # note: host:port or http://user:pwd@host:port
-# Please replace curl as "curl -x $http_proxy"
+
 path=`pwd`
 tempdir=`mktemp -td dir.XXXXXXXX`
 cd $tempdir
